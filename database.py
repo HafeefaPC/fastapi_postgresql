20d37526614548be94bdde2cbe2be67384a14534
Hafeefa <hafeefapc2003@gmail.com>
2024-06-25 14:09:31 +0530

    complete

--- database.py
+++ database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-URL_DATABASE= 'postgresql://postgres:moideenkutty@localhost:5432/QuizApp'
+URL_DATABASE= 'postgresql://postgres:@localhost:5432/QuizApp'
 
 engine = create_engine(URL_DATABASE)
 
